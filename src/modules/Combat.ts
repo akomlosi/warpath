6ec2a8b606385ed9ea6c8f9c1568eae3c5b18cde
ebolatumor <komlosi.attila@gmail.com>
2017-02-26 21:11:25 +0100

    more combat stuff

--- src/modules/Combat.ts
+++ src/modules/Combat.ts
@@ -9,23 +9,35 @@ export class Combat implements ICombat {
 	public static COMBAT_STATUS_INIT: number = 0;
 	public static COMBAT_STATUS_IN_PROGRESS: number = 1;
 	public static COMBAT_STATUS_ENDED: number = 2;
+	public static INITIAL_CREDITS_AMOUNT: number = 100;
 
-	public players: Array<Player>;
+	protected players: Array<Player>;
 	private maxPlayers: number;
+	private activePlayer: Player;
+	private status = Combat.COMBAT_STATUS_INIT;
 
 	constructor() {
 		this.maxPlayers = 2;
 		this.players = Array<Player>();
+		this.activePlayer = null;
 	};
 
-	addPlayer(player: Player): void {
+	public addPlayer(player: Player): void {
 		if (this.players.length < this.maxPlayers) {
 			this.players.push(player);
+			player.updateCredits(Combat.INITIAL_CREDITS_AMOUNT);
+			this.updateStatus();
 		}
 		else {
 			throw Error('Player list full');
 		}
 	};
 
+	private updateStatus(): void {
+		if (this.players.length === this.maxPlayers && this.status === Combat.COMBAT_STATUS_INIT) {
+			this.activePlayer = this.players[0];
+			this.status = Combat.COMBAT_STATUS_IN_PROGRESS;
+		}
+	};
 
 }
--- tests/Combat.spec.js
+++ tests/Combat.spec.js
@@ -1,17 +1,30 @@
 import {assert, expect} from 'chai';
+import sinon from 'sinon';
 import { Combat } from '../src/modules/Combat.js';
 
 let combat = null;
 
-let player1Mock = {};
-let player2Mock = {};
-let player3Mock = {};
+let player1Mock = {
+	updateCredits: ()=>{}
+};
+let player2Mock = {
+	updateCredits: ()=>{}
+};
+let player3Mock = {
+	updateCredits: ()=>{}
+};
 
 describe('Combat test', ()=> {
 	beforeEach(()=> {
-		combat = new Combat()
+		sinon.spy(player1Mock, 'updateCredits');
+		sinon.spy(player2Mock, 'updateCredits');
+		sinon.spy(player3Mock, 'updateCredits');
+		combat = new Combat();
 	});
 	afterEach(()=> {
+		player1Mock.updateCredits.restore();
+		player2Mock.updateCredits.restore();
+		player3Mock.updateCredits.restore();
 		combat = null;
 	});
 	
@@ -28,6 +41,11 @@ describe('Combat test', ()=> {
 		combat.addPlayer(player1Mock);
 		combat.addPlayer(player2Mock);
 		assert.throws(()=>{combat.addPlayer(player2Mock)}, Error, 'Player list full');
+	});
+
+	it('gives initial credits to players when join to combat', ()=>{
+		combat.addPlayer(player1Mock);
+		assert(player1Mock.updateCredits.calledOnce);
 	});
 
 });
